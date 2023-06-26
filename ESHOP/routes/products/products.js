ea410c0d270ba38ebb7b515922e4df2db71fbf9a
Lordzyblaze <104236692+Lordzyblaze@users.noreply.github.com>
2023-06-26 13:19:00 +0000

    products routes

--- ESHOP/routes/products/products.js
+++ ESHOP/routes/products/products.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {addProduct,getProduct,productProfile,productDelete } = require ("../../controllers/products/productctrl");
-
+const isLogin = require("../../middlewares/isLogin");
 const productsRouter = express.Router();
 
-productsRouter.post("/addproduct", addProduct);
+productsRouter.post("/addproduct", isLogin, addProduct);
 
-productsRouter.get("/", getProduct);
+productsRouter.get("/", isLogin, getProduct);
     
 productsRouter.get("/profile/:id", productProfile);
 productsRouter.delete("/:productId", productDelete);
