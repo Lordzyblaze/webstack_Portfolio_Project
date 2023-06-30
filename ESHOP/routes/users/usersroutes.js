33fffea6c65fad7269056b806e47fd7c1b1a4c90
Lordzyblaze <104236692+Lordzyblaze@users.noreply.github.com>
2023-06-30 07:32:09 +0000

    userroute

--- ESHOP/routes/users/usersroutes.js
+++ ESHOP/routes/users/usersroutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const storage = require("../../config/cloudinary");
 const isLogin = require("../../middlewares/isLogin");
-
-const {userRegister,userLogin,userProfile,userRetrive, updateUser,userDelete} = require ("../../controllers/users/usercrtl");
+const multer = require("multer")
+const {userRegister,userLogin,userProfile,userRetrive, updateUser,userDelete, profilePhotoUpload} = require ("../../controllers/users/usercrtl");
 
 const usersRouter = express.Router();
 
+// instance of multer
+const upload = multer ({storage});
 usersRouter.post("/register", userRegister);
         
 usersRouter.post("/login", userLogin);
@@ -14,6 +17,7 @@ usersRouter.get("/", userRetrive);
 
 usersRouter.put("/:userId", updateUser);
 usersRouter.delete("/:userId", userDelete);
+usersRouter.post("/profile-photo-upload",upload.single("profile"), profilePhotoUpload);
 
 
 
